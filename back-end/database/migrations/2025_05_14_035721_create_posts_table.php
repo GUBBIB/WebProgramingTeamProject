fa0563e1365a14cbf56924853e52e086296d5d2f
GUBBIB <moon.dragon250@gmail.com>
2025-05-14 21:55:32 +0900

    table update

--- back-end/database/migrations/2025_05_14_035721_create_posts_table.php
+++ back-end/database/migrations/2025_05_14_035721_create_posts_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
 
             $table->timestamps(); // created_at, updated_at 자동 생성 및 관리
 
+            $table->integer('PST_front_page')->nullable();
+            $table->integer('PST_behind_page')->nullable();
+
             // 외래 키 제약 조건
             $table->foreign('BRD_id')->references('BRD_id')->on('boards')->onDelete('cascade');
             $table->foreign('USR_id')->references('USR_id')->on('users')->onDelete('cascade');
