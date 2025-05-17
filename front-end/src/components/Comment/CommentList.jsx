76f0064d2ee8bb37dc776eb6939118bf9538a1b5
GUBBIB <moon.dragon250@gmail.com>
2025-05-18 05:20:26 +0900

    Upload

--- front-end/src/components/Comment/CommentList.jsx
+++ front-end/src/components/Comment/CommentList.jsx
@@ -1,34 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import CommentItem from './CommentItem';
+import CommentForm from './CommentForm'; // 댓글 작성 폼 import
 import './Comment.css';
 
-const CommentList = ({ BRD_id, PST_id }) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const response = await axios.get('http://13.60.93.77/boards/'+ BRD_id + '/posts/' + PST_id + '/comments');
-        setComments(response.data);
-      } catch (error) {
-        console.error('댓글 불러오기 실패:', error);
-      }
-    };
-
-    fetchComments();
-  }, [BRD_id, PST_id]);
-
-  if (!comments || comments.length === 0) {
-    return <p className="no-comments">아직 댓글이 없습니다.</p>;
-  }
-
+const CommentList = ({ comments, onSubmitComment, currentUser }) => {
   return (
     <div className="comment-list">
-      <h3 className="comment-list-title">댓글 ({comments.length})</h3>
-      {comments.map((comment) => (
+      <h3 className="comment-list-title">댓글 ({comments?.length || 0})</h3>
+
+      {(!comments || comments.length === 0) && (
+        <p className="no-comments">아직 댓글이 없습니다.</p>
+      )}
+
+      {comments?.map((comment) => (
         <CommentItem key={comment.COM_id} comment={comment} />
       ))}
+
+      {/* 댓글 입력 폼 추가 */}
+      <CommentForm
+        currentUser={currentUser}
+        onSubmitComment={onSubmitComment}
+      />
     </div>
   );
 };
