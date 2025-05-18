cb3ee927bc482edb5df6701c182531cafd6d5c4e
GUBBIB <moon.dragon250@gmail.com>
2025-05-18 23:29:15 +0900

    Upload

--- front-end/src/pages/PostWritePage.jsx
+++ front-end/src/pages/PostWritePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MDEditor from '@uiw/react-md-editor';
+import '@uiw/react-md-editor/dist/mdeditor.css';
 import './PostWritePage.css';
 
 const API_BASE_URL = 'http://13.60.93.77/api';
