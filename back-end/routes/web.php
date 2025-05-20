a2740a97c4fd2e4ddce8c1d87959770931048857
GUBBIB <moon.dragon250@gmail.com>
2025-05-20 23:19:53 +0900

    web.php 수정 주소에 /api 추가

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -8,13 +8,13 @@ Route::get('/', function () {
 });
 
 //누구나 접근 가능
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/api/register', [AuthController::class, 'register']);
+Route::post('/api/login', [AuthController::class, 'login']);
 
 // 내부적으로 auth 라는 middleware 가 작성 돼 있고 
 // 사용자만 접근 가능
 Route::middleware(['auth'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::post('/profile', [AuthController::class, 'updateProfile']);
+    Route::post('/api/logout', [AuthController::class, 'logout']);
+    Route::get('/api/user', [AuthController::class, 'user']);
+    Route::post('/api/profile', [AuthController::class, 'updateProfile']);
 });
